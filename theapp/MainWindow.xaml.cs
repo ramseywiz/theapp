b570c41596b388d1dbab3c644b4d5df7047752cf
ramseywiz <ramchandoklow@gmail.com>
2025-02-27 17:43:46 -0600

    progressed learning

--- theapp/MainWindow.xaml.cs
+++ theapp/MainWindow.xaml.cs
@@ -1,12 +1,28 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
 
 namespace theapp
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+        }
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public string BoundText
+        {
+            get { return boundText; }
+            set
+            {
+                boundText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+                //OnProperyChanged()
+            }
         }
     }
 }
